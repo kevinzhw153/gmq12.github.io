85569c780e9a7bb6188d40eb3349554f293527a1
gmq12 <68062598+gmq12@users.noreply.github.com>
2020-08-22 11:11:41 +0800

    Add files via upload

---
+++ script/blog.js
@@ -0,0 +1,21 @@
+var blog=[
+    ["博客/测试","../blog/test/","测试"]
+]
+function showBlogTitle() {
+    var left=document.getElementById("left");
+    for(var i=0;i < blog.length;i++) {
+        var div=document.createElement("div");
+        div.classList+="card";
+        var title=document.createElement("p");
+        title.style.fontSize="2em";
+        title.style.textAlign="center";
+        title.innerHTML="<a href='"+blog[i][1]+"' style='text-decoration:none;color:#2c2c6c;'>"+blog[i][0]+"</a>";
+        div.appendChild(title);
+        var subtitle=document.createElement("p");
+        subtitle.innerHTML="under "+blog[i][2];
+        subtitle.classList+="fblue";
+        subtitle.style.textAlign="center";
+        div.appendChild(subtitle);
+        left.appendChild(div);
+    }
+}
